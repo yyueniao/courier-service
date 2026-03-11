e1e23bafb847a444480471b4ff10c9590c687ea9
Say Peng <yyueniao0913@gmail.com>
2026-03-11 13:22:02 +0800

    feat: add new cli command `delivery-time`
    This command do the same thing as `delivery-cost` at this
    moment. We will implement the logic at the following commits.

---
+++ src/controllers/deliveryTime.ts
@@ -0,0 +1,48 @@
+import { stdin, stdout } from "node:process";
+import * as readline from "node:readline/promises";
+import { Package } from "../models.js";
+import { parsePositiveIntSafely } from "../utils.js";
+import services from "../services.js";
+
+export const deliveryTimeController = async (
+  baseDeliveryCostInString: string,
+  numberOfParcelsInString: string,
+) => {
+  const baseDeliveryCost = parsePositiveIntSafely(
+    baseDeliveryCostInString,
+    "base delivery cost",
+  );
+  const numberOfParcels = parsePositiveIntSafely(
+    numberOfParcelsInString,
+    "number of parcels",
+  );
+
+  const rl = readline.createInterface({ input: stdin, output: stdout });
+  const packages: Package[] = [];
+
+  console.log(`--- Starting Input for ${numberOfParcels} Packages ---`);
+
+  for (let i = 1; i <= numberOfParcels; i++) {
+    const packageInfo = await rl.question(`Please input package ${i} Info:\n`);
+    const [id, weight, distance, offerCode] = packageInfo.split(" ");
+    packages.push({
+      id,
+      weight: parsePositiveIntSafely(weight, "package weight"),
+      distance: parsePositiveIntSafely(distance, "delivery distance"),
+      offerCode,
+    });
+  }
+
+  rl.close();
+
+  console.log("\n--- Delivery Time ---");
+  packages.forEach((packageInfo) => {
+    const deliveryCostdto = services.getDeliveryCost(
+      baseDeliveryCost,
+      packageInfo,
+    );
+    console.log(
+      `${deliveryCostdto.id} ${deliveryCostdto.discount} ${deliveryCostdto.totalDeliveryCost}`,
+    );
+  });
+};
