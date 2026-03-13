8379144fff57ad50c139e0a11637735e094ab105
Say Peng <yyueniao0913@gmail.com>
2026-03-13 17:12:21 +0800

    refactor(controllers): extract `baseController`

--- src/controllers/deliveryTime.ts
+++ src/controllers/deliveryTime.ts
@@ -1,43 +1,13 @@
-import { stdin, stdout } from "node:process";
 import * as readline from "node:readline/promises";
 import { Package } from "../models.js";
 import { askForInput, parsePositiveIntSafely } from "../utils.js";
 import services from "../services.js";
 
 export const deliveryTimeController = async (
-  baseDeliveryCostInString: string,
-  numberOfParcelsInString: string,
+  rl: readline.Interface,
+  baseDeliveryCost: number,
+  packages: Package[],
 ) => {
-  const baseDeliveryCost = parsePositiveIntSafely(
-    baseDeliveryCostInString,
-    "base delivery cost",
-  );
-  const numberOfParcels = parsePositiveIntSafely(
-    numberOfParcelsInString,
-    "number of parcels",
-  );
-
-  const rl = readline.createInterface({ input: stdin, output: stdout });
-  const packages: Package[] = [];
-
-  console.log(`--- Starting Input for ${numberOfParcels} Packages ---`);
-
-  for (let i = 0; i < numberOfParcels; i++) {
-    const [id, weight, distance, offerCode] = await askForInput(
-      rl,
-      `Please input package ${i + 1} Info:\n`,
-      4,
-    );
-
-    packages.push({
-      index: i,
-      id,
-      weight: parsePositiveIntSafely(weight, "package weight"),
-      distance: parsePositiveIntSafely(distance, "delivery distance"),
-      offerCode,
-    });
-  }
-
   const [
     numberOfVehiclesInString,
     maxSpeedInString,
